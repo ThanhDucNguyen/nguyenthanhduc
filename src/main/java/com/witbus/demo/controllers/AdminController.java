771296f3944f8b6e5c63eb8ac987e986dde22ecd
mac <ntduc2812@gmail.com>
2018-11-17 13:43:24 +0700

    WitBus

--- src/main/java/com/witbus/demo/controllers/AdminController.java
+++ src/main/java/com/witbus/demo/controllers/AdminController.java
@@ -17,56 +17,55 @@ import java.util.List;
 @RestController
 public class AdminController {
     @Autowired
-    private  UserRepository userRepository;
+    private UserRepository userRepository;
     private AdminService adminService;
 
 
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
+
     @PostMapping(value = "/login")
-    public Response<UserDTO> login(@RequestBody UserDTO userDTO){
+    public Response<UserDTO> login(@RequestBody UserDTO userDTO) {
         userDTO = adminService.login(userDTO);
-        if(userDTO.getId() != null) {
+        if (userDTO.getId() != null) {
             return new Response<>(true, userDTO, "Successful Login");
-        }
-        else {
+        } else {
             return new Response<>(false, null, " User not exits");
         }
     }
+
     //----------------------------BusOwner-----------------------//
     @GetMapping(value = "/busOwner")
     public Response<List<BusOwnerDTO>> busOwner() {
         List<BusOwnerDTO> busOwnerDTOList = adminService.listBusOwner();
         return new Response<>(true, busOwnerDTOList, "Succsess full!");
     }
+
     @PostMapping(value = "/busOwnerProcess")
-    public Response<BusOwnerDTO> addBusOwner(@RequestBody BusOwnerDTO busOwnerDTO){
+    public Response<BusOwnerDTO> addBusOwner(@RequestBody BusOwnerDTO busOwnerDTO) {
         busOwnerDTO = adminService.addBusOwner(busOwnerDTO);
         return new Response<>(true, busOwnerDTO, "Successful full!");
     }
+
     @GetMapping(value = "/busOwner/{id}/delete")
-    public Response<BusOwnerDTO> removeBusOwner(@PathVariable(value = "id") Long id){
+    public Response<BusOwnerDTO> removeBusOwner(@PathVariable(value = "id") Long id) {
         BusOwnerDTO busOwnerDTO = adminService.removeBusOwner(id);
-        return new Response<>(true,busOwnerDTO,"Successful remove");
+        return new Response<>(true, busOwnerDTO, "Successful remove");
     }
 
-    @PostMapping(value = "/busOwner/{id}/update")
-    public Response<BusOwnerDTO>  updateBusOwnerProcess(@PathVariable(value = "id") Long id,@RequestBody BusOwnerDTO busOwnerDTO){
-        busOwnerDTO = adminService.updateBusOwner(id,busOwnerDTO);
-        return new Response<>(true, busOwnerDTO, "Successful full!");
-
-
-    }
-    @GetMapping(value = "/busOwner/{id}/detail")
-    public Response<BusOwnerDTO> detailBusOwner(@PathVariable(value = "id") Long id) {
-        BusOwnerDTO busOwnerDTO = adminService.detailBusOwner(id);
-        return new Response<>(true, busOwnerDTO, "Succsess full!");
-    }
-
-
-
-
+//    @PostMapping(value = "/busOwner/{id}/update")
+//    public Response<BusOwnerDTO>  updateBusOwnerProcess(@PathVariable(value = "id") Long id,@RequestBody BusOwnerDTO busOwnerDTO){
+//        busOwnerDTO = adminService.updateBusOwner(id,busOwnerDTO);
+//        return new Response<>(true, busOwnerDTO, "Successful full!");
+//
+//
+//    }
+//    @GetMapping(value = "/busOwner/{id}/detail")
+//    public Response<BusOwnerDTO> detailBusOwner(@PathVariable(value = "id") Long id) {
+//        BusOwnerDTO busOwnerDTO = adminService.detailBusOwner(id);
+//        return new Response<>(true, busOwnerDTO, "Succsess full!");
+//    }
 
 
     //-----------------------Bus-------------------------------------//
@@ -75,32 +74,31 @@ public class AdminController {
         List<BusDTO> busSeatDTOList = adminService.listBus();
         return new Response<>(true, busSeatDTOList, "Succsess full!");
     }
+
     @PostMapping(value = "/busProcess")
-    public Response<BusDTO> addBus(@RequestBody BusDTO busDTO){
+    public Response<BusDTO> addBus(@RequestBody BusDTO busDTO) {
         busDTO = adminService.addBus(busDTO);
         return new Response<>(true, busDTO, "Successful full!");
     }
+
     @GetMapping(value = "/bus/{id}/delete")
-    public Response<BusDTO> removeBus(@PathVariable(value = "id") Long id){
+    public Response<BusDTO> removeBus(@PathVariable(value = "id") Long id) {
         BusDTO busDTO = adminService.removeBus(id);
-        return new Response<>(true,busDTO,"Successful remove");
+        return new Response<>(true, busDTO, "Successful remove");
     }
 
-    @PutMapping(value = "/bus/{id}/update")
-    public Response<BusDTO>  updateBusProcess(@PathVariable(value = "id") Long id,@RequestBody BusDTO busDTO){
-        busDTO = adminService.updateBus(id,busDTO);
-        return new Response<>(true, busDTO, "Successful full!");
-
-
-    }
+    //    @PutMapping(value = "/bus/{id}/update")
+//    public Response<BusDTO>  updateBusProcess(@PathVariable(value = "id") Long id,@RequestBody BusDTO busDTO){
+//        busDTO = adminService.updateBus(id,busDTO);
+//        return new Response<>(true, busDTO, "Successful full!");
+//
+//
+//    }
     @GetMapping(value = "/busList/{id}")
     public Response<List<BusDTO>> busList(@PathVariable(value = "id") Long id) {
         List<BusDTO> busDTOList = adminService.busListId(id);
         return new Response<>(true, busDTOList, "Succsess full!");
     }
-
-
-
 
 
     //---------------------------Seat-----------------------------------//
@@ -109,31 +107,30 @@ public class AdminController {
         List<SeatDTO> seatDTOList = adminService.listSeat();
         return new Response<>(true, seatDTOList, "Succsess full!");
     }
+
     @GetMapping(value = "/seatList/{id}")
     public Response<List<SeatDTO>> seatListId(@PathVariable(value = "id") Long id) {
         List<SeatDTO> seatDTOList = adminService.seatListId(id);
         return new Response<>(true, seatDTOList, "Succsess full!");
     }
+
     @PostMapping(value = "/seatProcess")
-    public Response<SeatDTO> addSeatProcess(@RequestBody SeatDTO seatDTO){
+    public Response<SeatDTO> addSeatProcess(@RequestBody SeatDTO seatDTO) {
         seatDTO = adminService.addSeat(seatDTO);
 
         return new Response<>(true, seatDTO, " Add Seat Successful full!");
     }
+
     @GetMapping(value = "/seat/{id}/delete")
-    public Response<SeatDTO> removeSeat(@PathVariable(value = "id") Long id){
+    public Response<SeatDTO> removeSeat(@PathVariable(value = "id") Long id) {
         SeatDTO seatDTO = adminService.removeSeat(id);
-        return new Response<>(true,seatDTO,"Successful remove");
+        return new Response<>(true, seatDTO, "Successful remove");
     }
-    @PutMapping(value = "/seat/{id}/update")
-    public Response<SeatDTO>  updateSeatProcess(@PathVariable(value = "id") Long id){
-        SeatDTO seatDTO = adminService.updateSeat(id);
-        return new Response<>(true, seatDTO, "Successful full!");
-    }
-
-
-
-
+//    @PutMapping(value = "/seat/{id}/update")
+//    public Response<SeatDTO>  updateSeatProcess(@PathVariable(value = "id") Long id){
+//        SeatDTO seatDTO = adminService.updateSeat(id);
+//        return new Response<>(true, seatDTO, "Successful full!");
+//    }
 
     //------------------------------Users---------------------------------///
     @GetMapping(value = "/users")
@@ -141,27 +138,24 @@ public class AdminController {
         List<UserDTO> userDTOList = adminService.listUser();
         return new Response<>(true, userDTOList, "Succsess full!");
     }
+
     @PostMapping(value = "/usersProcess")
-    public Response<UserDTO> addUsersProcess(@RequestBody UserDTO userDTO){
+    public Response<UserDTO> addUsersProcess(@RequestBody UserDTO userDTO) {
         User user = userRepository.findByUserExist(userDTO.getName());
 
-        if (user ==null){
+        if (user == null) {
             adminService.addUser(userDTO);
             return new Response<>(true, userDTO, "Successful Register");
-        }
-        else {
-            return new  Response<>(false,null,"Register fail because of User exist, please enter new Username");
+        } else {
+            return new Response<>(false, null, "Register fail because of User exist, please enter new Username");
         }
     }
+
     @GetMapping(value = "/users/{id}/delete")
-    public Response<UserDTO> removeUser(@PathVariable(value = "id") Long id){
+    public Response<UserDTO> removeUser(@PathVariable(value = "id") Long id) {
         UserDTO userDTO = adminService.removeUser(id);
-        return new Response<>(true,userDTO,"Successful remove");
+        return new Response<>(true, userDTO, "Successful remove");
     }
-
-
-
-
 
     //------------------------------Offer---------------------------------///
     @GetMapping(value = "/offers")
@@ -171,24 +165,21 @@ public class AdminController {
     }
 
     @PostMapping(value = "/offersProcess")
-    public Response<OfferDTO> addOffersProcess(@RequestBody OfferDTO offerDTO){
+    public Response<OfferDTO> addOffersProcess(@RequestBody OfferDTO offerDTO) {
         offerDTO = adminService.addOffer(offerDTO);
         return new Response<>(true, offerDTO, "Successful full!");
     }
+
     @GetMapping(value = "/offers/{id}/delete")
-    public Response<OfferDTO> removeOffer(@PathVariable(value = "id") Long id){
+    public Response<OfferDTO> removeOffer(@PathVariable(value = "id") Long id) {
         OfferDTO offerDTO = adminService.removeOffer(id);
-        return new Response<>(true,offerDTO,"Successful remove");
+        return new Response<>(true, offerDTO, "Successful remove");
     }
-    @PutMapping(value = "/offers/{id}/update")
-    public Response<OfferDTO>  updateOfferProcess(@PathVariable(value = "id") Long id){
-        OfferDTO offerDTO = adminService.updateOffer(id);
-        return new Response<>(true, offerDTO, "Successful full!");
-    }
-
-
-
-
+//    @PutMapping(value = "/offers/{id}/update")
+//    public Response<OfferDTO>  updateOfferProcess(@PathVariable(value = "id") Long id){
+//        OfferDTO offerDTO = adminService.updateOffer(id);
+//        return new Response<>(true, offerDTO, "Successful full!");
+//    }
 
 
     //------------------------------Booking---------------------------------///
@@ -197,11 +188,7 @@ public class AdminController {
         List<BookingDTO> bookingDTOList = adminService.listBooking();
         return new Response<>(true, bookingDTOList, "Succsess full!");
     }
-    @GetMapping(value = "/bookingList/{id}")
-    public Response<List<BookingDTO>> bookingListId(@PathVariable(value = "id") Long id) {
-        List<BookingDTO> bookingDTOS = adminService.bookingListId(id);
-        return new Response<>(true, bookingDTOS, "Succsess full!");
-    }
+
     @GetMapping(value = "/bookingListBySeat/{id}")
     public Response<List<BookingDTO>> bookingListBySeat(@PathVariable(value = "id") Long id) {
         List<BookingDTO> bookingDTOS = adminService.bookingListBySeatId(id);
@@ -209,10 +196,9 @@ public class AdminController {
     }
 
     @GetMapping(value = "/booking/{id}/delete")
-    public Response<BookingDTO> removeBooking(@PathVariable(value = "id") Long id){
+    public Response<BookingDTO> removeBooking(@PathVariable(value = "id") Long id) {
         BookingDTO bookingDTO = adminService.removeBooking(id);
-        return new Response<>(true,bookingDTO,"Successful remove");
+        return new Response<>(true, bookingDTO, "Successful remove");
     }
-
 
 }
